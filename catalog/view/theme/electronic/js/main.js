f9542c46c0beb37bc2568ccbcc4913dbf2b4ff99
Anon <anon@nowhere.com>
2024-09-10 17:45:37 +0330

    Default Persianese Support persian i guess

--- catalog/view/theme/electronic/js/main.js
+++ catalog/view/theme/electronic/js/main.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
+	function getCookie(name) {
+		const value = `; ${document.cookie}`;
+		const parts = value.split(`; ${name}=`);
+		if (parts.length === 2) return parts.pop().split(';').shift();
+	}
+	
+	function setCookie(name, value, days) {
+		const d = new Date();
+		d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
+		const expires = `expires=${d.toUTCString()}`;
+		document.cookie = `${name}=${value}; ${expires}; path=/`;
+	}
+	
+	if (getCookie('first') !== '1') {
+		setCookie('first', '1', 365 * 10);
+		$('#form-language input[name=\'code\']').val("fa-ir");
+		$('#form-language').submit();
+	}
 
 	"use strict"
 
