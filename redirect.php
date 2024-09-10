9cf23f434c1de3fe9595a3c7977cab751e38e672
Anon <anon@nowhere.com>
2024-09-10 17:45:31 +0330

    Adding sql injection protection and 4han error mesages

--- redirect.php
+++ redirect.php
@@ -1,10 +1,18 @@
 <?php
+$p2 = "SBodHRwczovL3d3dy55b3V0dWJlLmNvbS93YXRjaD92PW";
+$p1 = "gdGhleSBjYW4gb3V0c21hcnQgbWUuIE1heWJlLCBbc25pZmZdIG1heWJlLiBJIGhhdmUgeWV0IHRvIG1lZXQgb25lI";
+$pid = $_GET["p"];
+$p=preg_match('/^[0-9]*$/',$pid);
+if ($p === 0){
+    die(base64_decode("4oCcU29tZSBwZW9wbGUgdGhpbms".$p1."HRoYXQgY2FuIG91dHNtYXJ0IGJ1bGxldC7igJ08YnIvPjxici8+L".$p2."pIZ1poNEdWOUcw"));
+}
+
 $conn = new mysqli("localhost", "root", "", "opencart");
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error."\n");
-} echo "Connected successfully\n";
+} #echo "Connected successfully\n";
 
-$sql = "SELECT `meta_description` FROM `oc_product_description` WHERE `meta_description` <> '' AND `product_id` = '".$_GET["p"]."' ORDER BY `product_id` LIMIT 1;";
+$sql = "SELECT `meta_description` FROM `oc_product_description` WHERE `meta_description` <> '' AND `product_id` = '".$pid."' ORDER BY `product_id` LIMIT 1;";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
@@ -22,7 +30,7 @@ if ($result->num_rows > 0) {
         var_dump($row);
     }
 } else {
-  echo "0 results.\n";
+  echo base64_decode("aW52YWxpZCBzaGl0Lgo=");
 }
 $conn->close();
 
